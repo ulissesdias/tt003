9ff276537525b542314fe4cc0b99cfb6ab4c35a6
Ulisses Martins Dias <ulisses@Ulissess-MacBook-Air.local>
2023-11-25 08:56:54 -0300

    segunda versao de breakthrough

--- libraries/breakthrough.py
+++ libraries/breakthrough.py
@@ -65,7 +65,24 @@ def winner(lines, columns, white_positions, black_positions) :
     ## Jogo ainda não acabou
     return 0
 
-def valid_move(move, lines, columns, white_positions, black_positions) :
+## Recebe um tabuleiro e devolve ele girado
+def flip_board(lines, columns, white_positions, black_positions) :
+    new_white = []
+    new_black = []
+
+    for coord in black_positions :
+        new_white.append(
+            ( lines-coord[0]+1, columns-coord[1]+1)
+        )
+    for coord in white_positions :
+        new_black.append(
+            ( lines-coord[0]+1, columns-coord[1]+1)
+        )
+    return lines, columns, tuple(new_white), tuple(new_black)
+
+
+## Verificar se temos uma jogada valida para as brancas
+def valid_white_move(move, lines, columns, white_positions, black_positions) :
     ## Se não houver move, então já saia.
     if not move :
         return False
@@ -102,8 +119,8 @@ def valid_move(move, lines, columns, white_positions, black_positions) :
             return False
     return True
     
-def get_valid_moves(lines, columns, white_positions, black_positions) :
-    valid_moves = []
+def get_valid_white_moves(lines, columns, white_positions, black_positions) :
+    valid_white_moves = []
     for origem in white_positions :
         destinos = (
             (origem[0]+1, origem[1]-1),
@@ -111,32 +128,62 @@ def get_valid_moves(lines, columns, white_positions, black_positions) :
             (origem[0]+1, origem[1]+1)
         )
         for destino in destinos :
-            if valid_move( (origem, destino), lines, columns, white_positions, black_positions) :
-               valid_moves.append(  (origem, destino)  )
-    return tuple(valid_moves)
+            if valid_white_move( (origem, destino), lines, columns, white_positions, black_positions) :
+               valid_white_moves.append(  (origem, destino)  )
+    return tuple(valid_white_moves)
 
-
+def get_valid_moves(lines, columns, white_positions, black_positions, turn) :
+    if turn == 1 : ## Vez das brancas
+        return get_valid_white_moves(lines, columns, white_positions, black_positions)
+    else :
+        ltabuleiro = flip_board(lines, columns, white_positions, black_positions)
+        lines, columns, white_positions, black_positions = ltabuleiro
+        flipped_moves = get_valid_white_moves(lines, columns, white_positions, black_positions)
+        moves = []
+        for move in flipped_move :
+            new_move = []
+            for coord in move :
+                new_move.append(
+                    ( lines-coord[0]+1, columns-coord[1]+1)
+                )
+            moves.append(new_move)
+        return moves
+        
 ## A função abaixo é um jogador. Ele receberá um tabuleiro
 ## como parâmetro pelas variáveis  lines, columns, white_positions,
 ## black_positions.
-def rnadom_player(lines, columns, white_positions, black_positions) :
-    valid_moves = get_valid_moves(lines, columns, white_positions, black_positions)
+def rnadom_player(lines, columns, white_positions, black_positions, turn) :
+    valid_moves = get_valid_moves(lines, columns, white_positions, black_positions, turn)
     if valid_moves :
         return random.choice(valid_moves)
 
-
-def human_player(lines, columns, white_positions, black_positions) :
+def human_player(lines, columns, white_positions, black_positions, turn) :
     board = draw_board(lines, columns, white_positions, black_positions)
     display(board)
     print("Jogue no formato '(origemLine, origemColumn), (destinoLine, destinoColumn)' ")
     origem, destino = eval(input())
     return origem, destino
-    
+
+
+## O parâmetro flip_black_board será usado mais adiante.
+def generate_image_history(history, flip_black_board = False) :
+    image_history = []
+    lines, columns = history[0]
+
+    for i in range(1, len(history)) :
+        white_positions, black_positions = history[i]
+        if ( flip_black_board and (i % 2 == 1) ) :
+            lines, columns, white_positions, black_positions = flip_board(lines, columns, white_positions, black_positions)
+        image_history.append( draw_board(lines, columns, white_positions, black_positions ) )
+    return image_history
+
+
+
 ## Abaixo uma função que gerencia um jogo.
 def game(player1, player2, lines = 5, columns = 5,
          white_positions = ( (1,1), (1,2), (1,3), (1,4), (1,5) ),
          black_positions = ( (5,1), (5,2), (5,3), (5,4), (5,5) ) ) :
-
+    
     ## Inicialização
     turn   = 1
     result = 0
@@ -150,7 +197,7 @@ def game(player1, player2, lines = 5, columns = 5,
 
         ## Pedindo uma jogada ao jogador da vez.
         if turn == 1 :
-            origem, destino = player1(lines, columns, white_positions, black_positions)
+            origem, destino = player1(lines, columns, white_positions, black_positions, turn)
 
             lwhite_positions = list(white_positions)
             lwhite_positions.remove(origem)
@@ -163,7 +210,7 @@ def game(player1, player2, lines = 5, columns = 5,
                 black_positions = tuple(lblack_positions)
 
         else :
-            origem, destino = player2(lines, columns, white_positions, black_positions)
+            origem, destino = player2(lines, columns, white_positions, black_positions, turn)
 
             lblack_positions = list(black_positions)
             lblack_positions.remove(origem)
